2e5c92667877bdf8cfd07af57025e73f7b81019c
Ahmed <wwpplldd44@gmail.com>
2025-05-02 05:00:33 +0300

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"], // حسب مكان ملفاتك
+  theme: {
+    extend: {
+      colors: {
+        brand: '#00B288', // لون مخصص
+      },
+      // fontFamily: {
+      //   arabic: ['Cairo', 'sans-serif'], // خط مخصص
+      // },
+      // spacing: {
+      //   '72': '18rem', // حجم مخصص للمسافة
+      // },
+    },
+  },
+  plugins: [],
+};
+
+  
+    
+  
